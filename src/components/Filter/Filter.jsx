b34a2b8058ba2452e0a0781f359be6f431f546ec
Kateryna-Borysenko <k.borysenko.kyiv@gmail.com>
2022-01-27 16:24:04 +0200

    Added useRef hook to Filter

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,12 +1,19 @@
+import { useEffect, useRef } from 'react';
 import s from './Filter.module.css';
+
 // import PropTypes from 'prop-types';
 
 const Filter = ({ value, onChange }) => {
+  const inputRef = useRef(null);
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
   return (
     <div>
       <label className={s.label}>
         <span className={s.title}>Find contacts by name</span>
         <input
+          ref={inputRef}
           className={s.textField}
           type="text"
           name="filter"
